ebd7af7734a77e7ced5f69614d390551e266ecb6
yassineelmiri <yassireelhima@gmail.com>
2024-02-05 09:56:16 +0100

    project de Movies

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', 'MoviesController@index')->name('movies.index');
+Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tv', 'TvController@index')->name('tv.index');
+Route::get('/tv/{id}', 'TvController@show')->name('tv.show');
+
+Route::get('/actors', 'ActorsController@index')->name('actors.index');
+Route::get('/actors/page/{page?}', 'ActorsController@index');
+
+Route::get('/actors/{id}', 'ActorsController@show')->name('actors.show');
